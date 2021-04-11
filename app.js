c632071e15a1701fbf24bede8d2c73a3829a1e30
betilla x <aclara_77@yahoo.com>
2021-04-11 18:20:13 +0100

    complete rota for the week function

---
+++ app.js
@@ -0,0 +1,55 @@
+document.addEventListener("click", generateRota);
+
+
+let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+let squad = [
+  "A La Cart",
+  "Bionicles",
+  "Brickoin",
+  "Cybots",
+  "Disco",
+  "Dora",
+  "Guardians",
+  "Houston",
+  "Mixels",
+  "VIPers",
+];
+
+let daysOfWeekCounter = 0;
+
+function generateDay() {
+  console.log(squad.length);
+
+  let newSquad = [];
+  console.log(newSquad.length);
+
+  let randomNumber = Math.floor(Math.random() * squad.length);
+  console.log(randomNumber);
+
+  console.log(daysOfWeek[daysOfWeekCounter] + ": " + squad[randomNumber]);
+
+ document.querySelector(".answer").innerHTML +=  daysOfWeek[daysOfWeekCounter] + ": " + squad[randomNumber]+"<br>";
+
+
+
+  for (i = 0; i < squad.length; i++) {
+    if (i !== randomNumber) {
+      newSquad.push(squad[i]);
+    }
+  }
+
+  console.log(newSquad);
+
+  squad = newSquad;
+  console.log("this is the new array " + squad);
+
+  daysOfWeekCounter = daysOfWeekCounter + 1;
+
+  console.log(daysOfWeekCounter);
+}
+
+function generateRota() {
+  for (d = 0; d <= 4; d++) {
+    generateDay();
+  }
+}

9a09bb8754682aa868511799d81b561e536bd644
betilla x <aclara_77@yahoo.com>
2021-04-18 13:57:32 +0100

    add initialise and clear rota function

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-document.querySelector("button").addEventListener("click", generateRota);
+//declaring variables
 
-
-let daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-let squad = [
-  "A La Cart",
-  "Bionicles",
-  "Brickoin",
-  "Cybots",
-  "Disco",
-  "Dora",
-  "Guardians",
-  "Houston",
-  "Mixels",
-  "VIPers",
-];
-
+let daysOfWeek = [];
+let squad = [];
 let daysOfWeekCounter = 0;
+
+//initialise page
+initPage();
+
+//setting event listener for the buttons
+
+document.querySelector(".rota").addEventListener("click", generateRota);
+document.querySelector(".clear").addEventListener("click", initPage);
+
+//function to set initial variable, clear the tex in the div - also use to reset 
+
+function initPage() {
+  document.querySelector(".clear").classList.add("hidden");
+  document.querySelector(".answer").innerHTML = "";
+  daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  squad = [
+    "A La Cart",
+    "Bionicles",
+    "Brickoin",
+    "Cybots",
+    "Disco",
+    "Dora",
+    "Guardians",
+    "Houston",
+    "Mixels",
+    "VIPers",
+  ];
+
+  daysOfWeekCounter = 0;
+
+}
+
+
+
+// function to generate a day, write the answer in the div and create a new array without the squad selected
 
 function generateDay() {
   console.log(squad.length);
@@ -28,9 +50,8 @@ function generateDay() {
 
   console.log(daysOfWeek[daysOfWeekCounter] + ": " + squad[randomNumber]);
 
- document.querySelector(".answer").innerHTML +=  daysOfWeek[daysOfWeekCounter] + ": " + squad[randomNumber]+"<br>";
-
-
+  document.querySelector(".answer").innerHTML +=
+    daysOfWeek[daysOfWeekCounter] + ": " + squad[randomNumber] + "<br>";
 
   for (i = 0; i < squad.length; i++) {
     if (i !== randomNumber) {
@@ -48,8 +69,12 @@ function generateDay() {
   console.log(daysOfWeekCounter);
 }
 
+//function to generate a 5 days rota and display the clear button
+
 function generateRota() {
   for (d = 0; d <= 4; d++) {
     generateDay();
   }
+  document.querySelector(".clear").classList.remove("hidden");
 }
+
